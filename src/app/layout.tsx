bf30423a14cf3da897d90d9e5f4de386f3a51a0b
Gourav Modi <gmodi@cutx.org>
2025-07-14 13:31:54 +0530

    fix/unwanted-things-removed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,13 @@
 // app/layout.tsx
-import { AuthProvider } from '../context/AuthContext'
-import { QuestionProvider } from '../context/QuestionContext'
-
 import './globals.css'
-import Navbar from './navbar'
-import NavInner from './components/NavInner'
+// import NavInner from './components/NavInner'
 import { Toaster } from 'react-hot-toast';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
-        <QuestionProvider>
-          <AuthProvider>
-            <NavInner>
             {children}
-            </NavInner>
-          </AuthProvider>
-        </QuestionProvider>
         <Toaster position="top-right" />
       </body>
     </html>
